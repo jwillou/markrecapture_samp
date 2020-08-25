63d31750f592bc48ab899b3fb52946afbebaf97f
Janna Willoughby <jwilloughby@auburn.edu>
2020-08-25 16:57:53 -0500

    updated plotting and added output

--- sampling power.R
+++ sampling power.R
@@ -3,7 +3,7 @@ library(scales)
 
 ####power analysis for mark recapture####
 popsize = 2000000
-collects = recollects = c(seq(500, 5000, 500))
+collects = recollects = c(seq(500, 5000, 500), seq(7500, 50000, 5000))
 
 #output dataframe
 OUT = NULL
@@ -29,7 +29,7 @@ for(c in 1:length(collects)){
   #add these values to others
   OUT = rbind(OUT, writeout)
 }
-colnames(OUT) = c("collect", "recollect", "NstM", "NstSD")
+colnames(OUT) = c("collect", "recollect", "NstM", "NstSD", "NstUL", "NstLL")
 rownames(OUT) = seq(1,nrow(OUT), 1)
 o = as.data.frame(OUT)
 o$NstSE = 1.96*o$NstSD/sqrt(i)
@@ -37,11 +37,12 @@ o$totalgenos = o$collect + o$recollect
 
 #plot data nicely
 off = 100
-plot(-1000, -1000, xlim=c(0, max(collects*2)), ylim=c(0,2500000), xlab="total number of samples genotyped per year", ylab="estimated population size")
-segments(x0=0, x1=max(collects*2), y0=popsize, y1=popsize, lty=2, col="grey50")
+plot(-10000, -10000, xlim=c(0, 100000), ylim=c(0,3000000), xlab="total number of samples genotyped per year", ylab="estimated population size")
+segments(x0=0, x1=100000, y0=popsize, y1=popsize, lty=2, col="grey50")
 lines(x=o$totalgenos, y=o$NstM, lty=1, col=alpha("firebrick3", 0.5), lwd=2)
 points(x=o$totalgenos, y=o$NstM, pch=19, col=alpha("firebrick3", 0.5), cex=1.5)
-segments(x0=c(o$totalgenos - off), x1=c(o$totalgenos + off), y0=o$NstM-o$NstSE, y1=o$NstM-o$NstSE, col=alpha("firebrick3", 0.8))
-segments(x0=c(o$totalgenos - off), x1=c(o$totalgenos + off), y0=o$NstM+o$NstSE, y1=o$NstM+o$NstSE, col=alpha("firebrick3", 0.8))
-segments(x0=o$totalgenos, x1=o$totalgenos, y0=o$NstM-o$NstSE, y1=o$NstM+o$NstSE, col=alpha("firebrick3", 0.8))
+segments(x0=c(o$totalgenos - off), x1=c(o$totalgenos + off), y0=o$NstLL, y1=o$NstLL, col=alpha("firebrick3", 0.8))
+segments(x0=c(o$totalgenos - off), x1=c(o$totalgenos + off), y0=o$NstUL, y1=o$NstUL, col=alpha("firebrick3", 0.8))
+segments(x0=o$totalgenos, x1=o$totalgenos, y0=o$NstLL, y1=o$NstUL, col=alpha("firebrick3", 0.8))
+
 
